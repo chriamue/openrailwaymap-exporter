489ae13ef587b3209651e1e109c2bc2638104319
Christian M <chriamue@gmail.com>
2023-04-10 13:17:08 +0200

    :sparkles: renders cube on kiss3d component

--- src/app/kiss3d_component.rs
+++ src/app/kiss3d_component.rs
@@ -1,33 +1,54 @@
+use kiss3d::light::Light;
+use kiss3d::nalgebra::{Point3, Vector3};
+use kiss3d::scene::SceneNode;
+use kiss3d::window::State;
+use kiss3d::window::Window;
+use std::borrow::BorrowMut;
 use std::cell::RefCell;
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
-use web_sys::{Document, HtmlCanvasElement, Window};
 use yew::prelude::*;
 
 pub struct Kiss3dComponent {
 }
 
-pub enum Msg {
+#[derive(Clone)]
+struct AppState {
+    scene: SceneNode,
 }
+
+impl State for AppState {
+    fn step(&mut self, window: &mut Window) {}
+}
+
+pub enum Msg {}
 
 impl Component for Kiss3dComponent {
     type Message = Msg;
     type Properties = ();
 
-    fn create(ctx: &Context<Self>) -> Self {
-        Kiss3dComponent {}
+    fn create(_: &Context<Self>) -> Self {
+        Self {}
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, _: &Context<Self>, _: Self::Message) -> bool {
         false
     }
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
-
+    fn view(&self, _: &Context<Self>) -> Html {
         html! {
-            <>
-                <canvas id="canvas" width="800" height="600"></canvas>
-            </>
+            <canvas id="canvas" width="800" height="600"></canvas>
+        }
+    }
+
+    fn rendered(&mut self, _ctx: &Context<Self>, first_render: bool) {
+        if first_render {
+            let mut window = Window::new("Kiss3d + Yew");
+            window.set_light(Light::StickToCamera);
+            let mut cube = window.add_cube(1.0, 1.0, 1.0);
+            cube.set_color(0.0, 0.0, 1.0);
+            let state = AppState { scene: cube };
+            window.render_loop(state);
         }
     }
 }
