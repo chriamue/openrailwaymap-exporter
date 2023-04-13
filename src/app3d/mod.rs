a38b8e9f0b17370867c4c9248c3f18bacf8ddd62
Christian M <chriamue@gmail.com>
2023-04-13 09:21:55 +0200

    :sparkles: adds select of node in app3d

--- src/app3d/mod.rs
+++ src/app3d/mod.rs
@@ -4,20 +4,17 @@
 //! and provides systems for displaying the graph, interacting with the user interface, and updating the camera.
 //!
 #![cfg_attr(target_arch = "wasm32", allow(dead_code, unused_imports))]
-use std::sync::Arc;
-
 use crate::prelude::OverpassApiClient;
 use crate::prelude::OverpassImporter;
 use crate::prelude::RailwayApiClient;
 use crate::prelude::RailwayGraph;
 use crate::prelude::RailwayGraphImporter;
+use bevy::input::Input;
 use bevy::prelude::shape::Circle;
 use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
-
 use bevy_pancam::{PanCam, PanCamPlugin};
-
 use geo_types::coord;
 use petgraph::visit::IntoNodeReferences;
 use petgraph::visit::NodeRef;
@@ -33,7 +30,9 @@ use projection::Projection;
 struct Edge;
 // Components
 #[derive(Component)]
-struct Node;
+struct Node {
+    id: i64,
+}
 
 // Resources
 #[derive(Default, Resource)]
@@ -41,6 +40,12 @@ struct AppResource {
     area_name: String,
     graph: Option<RailwayGraph>,
     look_at_position: Option<Vec3>,
+}
+
+#[derive(Default, Resource)]
+struct SelectedNode {
+    pub start_node_id: Option<i64>,
+    pub end_node_id: Option<i64>,
 }
 
 /// Initializes the Bevy application with a given `RailwayGraph`.
@@ -90,10 +95,12 @@ pub fn init() {
         .add_plugin(EguiPlugin)
         .add_plugin(PanCamPlugin::default())
         .insert_resource(AppResource::default())
+        .insert_resource(SelectedNode::default())
         .insert_resource(projection)
         .add_startup_system(setup)
         .add_system(ui_system)
         .add_system(update_look_at_position_system)
+        .add_system(select_node_system)
         .run()
 }
 
@@ -114,8 +121,24 @@ fn ui_system(
     mut projection: ResMut<Projection>,
     meshes: ResMut<Assets<Mesh>>,
     materials: ResMut<Assets<ColorMaterial>>,
+    selected_node: Res<SelectedNode>,
 ) {
     egui::Window::new("").show(contexts.ctx_mut(), |ui| {
+        if let Some(node_id) = selected_node.start_node_id {
+            if let Some(graph) = &app_resource.graph {
+                display_selected_node_info(ui, graph, node_id);
+            }
+        } else {
+            ui.label("No node selected");
+        }
+        if let (Some(start_node_id), Some(end_node_id)) =
+            (selected_node.start_node_id, selected_node.end_node_id)
+        {
+            if let Some(graph) = &app_resource.graph {
+                display_path_info(ui, graph, start_node_id, end_node_id);
+            }
+        }
+
         ui.label("Enter an Area:");
         ui.text_edit_singleline(&mut app_resource.area_name);
 
@@ -155,6 +178,32 @@ fn ui_system(
             });
         }
     });
+}
+
+fn display_selected_node_info(ui: &mut egui::Ui, graph: &RailwayGraph, node_id: i64) {
+    if let Some(node_index) = graph.node_indices.get(&node_id) {
+        let node = &graph.graph[*node_index];
+        ui.label(format!("ID: {}", node.id));
+        ui.label(format!("Latitude: {}", node.lat));
+        ui.label(format!("Longitude: {}", node.lon));
+    }
+}
+
+fn display_path_info(
+    ui: &mut egui::Ui,
+    graph: &RailwayGraph,
+    start_node_id: i64,
+    end_node_id: i64,
+) {
+    if let (Some(start_node_index), Some(end_node_index)) = (
+        graph.node_indices.get(&start_node_id),
+        graph.node_indices.get(&end_node_id),
+    ) {
+        let start_node = &graph.graph[*start_node_index];
+        ui.label(format!("Start: {}", start_node.id));
+        let end_node = &graph.graph[*end_node_index];
+        ui.label(format!("End: {}", end_node.id));
+    }
 }
 
 fn update_look_at_position_system(
@@ -270,8 +319,49 @@ fn display_graph(
                         transform: Transform::from_translation(position),
                         ..default()
                     })
-                    .insert(Node);
+                    .insert(Node { id: node_data.id });
             }
+        }
+    }
+}
+
+fn select_node_system(
+    mut selected_node: ResMut<SelectedNode>,
+    mouse_button_inputs: Res<Input<MouseButton>>,
+    mut windows: Query<&mut Window>,
+    q_node: Query<(Entity, &Node, &Transform)>,
+    camera_q: Query<(&Camera, &GlobalTransform)>,
+) {
+    if mouse_button_inputs.just_pressed(MouseButton::Left) {
+        let window = windows.single_mut();
+        let (camera, camera_transform) = camera_q.single();
+
+        let mut closest = None;
+        let mut min_distance = f32::MAX;
+
+        for (entity, node, transform) in q_node.iter() {
+            if let Some(world_position) = window
+                .cursor_position()
+                .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+                .map(|ray| ray.origin)
+            {
+                let world_position_2d = Vec2::new(world_position.x, world_position.y);
+                let transform_2d = Vec2::new(transform.translation.x, transform.translation.y);
+                let distance = world_position_2d.distance(transform_2d);
+
+                if distance < 15.0 && distance < min_distance {
+                    min_distance = distance;
+                    closest = Some((entity, node.id));
+                }
+            }
+        }
+
+        if let Some((entity, id)) = closest {
+            println!("Selected node: {:?}", entity);
+            selected_node.end_node_id = selected_node.start_node_id;
+            selected_node.start_node_id = Some(id);
+        } else {
+            selected_node.start_node_id = None;
         }
     }
 }
