d231579ab25e8b9ecc37b935c4747929c7052fa2
Christian M <chriamue@gmail.com>
2023-06-15 20:49:33 +0200

    :bug: shows path in red

--- src/app3d/edges.rs
+++ src/app3d/edges.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_polyline::prelude::PolylineMaterial;
 
 use crate::{
     railway_algorithms::{PathFinding, RailwayGraphAlgos},
@@ -15,9 +16,9 @@ pub struct Edge {
 
 pub fn show_edges_on_path(
     app_resource: Res<AppResource>,
-    mut edge_query: Query<(&Edge, &mut Handle<StandardMaterial>)>,
+    mut edge_query: Query<(&Edge, &mut Handle<PolylineMaterial>)>,
     selected_node: Res<SelectedNode>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut polyline_materials: ResMut<Assets<PolylineMaterial>>,
 ) {
     if let Some(graph) = &app_resource.graph {
         if let (Some(start_node_id), Some(end_node_id)) =
@@ -36,19 +37,12 @@ pub fn show_edges_on_path(
                     let is_path_edge = path_edge_ids
                         .iter()
                         .any(|railway_edge| *railway_edge == edge_data.id);
-                    if let Some(material) = materials.get_mut(&material_handle) {
-                        material.base_color = if is_path_edge {
+                    if let Some(material) = polyline_materials.get_mut(&material_handle) {
+                        material.color = if is_path_edge {
                             Color::RED
                         } else {
                             Color::BLUE
                         };
-                    } else if is_path_edge {
-                        let new_material = StandardMaterial {
-                            base_color: Color::RED,
-                            ..Default::default()
-                        };
-                        let new_material_handle = materials.add(new_material);
-                        *material_handle = new_material_handle;
                     }
                 }
             }
