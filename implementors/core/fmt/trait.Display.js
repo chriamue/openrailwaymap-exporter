744032ad03671ec76682905b6e1b0ae3d47b957b
chriamue <chriamue@users.noreply.github.com>
2023-08-19 07:53:14 +0000

    deploy: 71d8cd4ad6d7b55873b960eb15f026bbc855dd10

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"openrailwaymap_exporter":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"openrailwaymap_exporter/simulation/agents/enum.RailMovableAction.html\" title=\"enum openrailwaymap_exporter::simulation::agents::RailMovableAction\">RailMovableAction</a>"]]
+"openrailwaymap_exporter":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"openrailwaymap_exporter/simulation/agents/enum.RailMovableAction.html\" title=\"enum openrailwaymap_exporter::simulation::agents::RailMovableAction\">RailMovableAction</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
